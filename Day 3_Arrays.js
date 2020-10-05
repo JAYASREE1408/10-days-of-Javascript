916b7c281b572cc9965bfeba98e114857279e81d
JAYASREE T <jayasreet2112@gmail.com>
2020-10-05 16:50:48 +0530

    Arrays

---
+++ Day 3_Arrays.js
@@ -0,0 +1,16 @@
+function getSecondLargest(nums) {
+    // getting the maximum value
+    let max=0;
+    for(let i of nums){
+        if(max<i){
+            max=i;
+        }
+    }
+    let max1=0;
+    for(let i of nums){
+        if(max1<i && i<max){
+            max1=i;
+        }
+    }
+    return max1;
+}
