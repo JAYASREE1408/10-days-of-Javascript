bcacb0237fe5b6e37a4967fb2bef7dfb286cfeac
JAYASREE T <jayasreet2112@gmail.com>
2020-12-02 23:00:41 +0530

    Update Day 0_Hello Wolrd.js

--- Day 0_Hello Wolrd.js
+++ Day 0_Hello Wolrd.js
@@ -4,7 +4,7 @@ process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 
 let inputString = ''; 
-let currentLine = 0;
+let currentLine = 0
   
 process.stdin.on('data', inputStdin => {
     inputString += inputStdin; 
